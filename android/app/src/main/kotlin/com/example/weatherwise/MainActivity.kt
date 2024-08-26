21b3619e1c4a771e357cf01f668962d21102f7a3
yasin <yasinmiapolash@gmail.com>
2024-08-26 23:34:29 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/weatherwise/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.weatherwise
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:weatherwise/view/splash/splash_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_ , child) {
+        return GetMaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'WeatherWise',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+            textTheme: Typography.englishLike2018.apply(fontSizeFactor: 1.sp),
+          ),
+          home: const SplashScreen(),
+        );
+      },
+
+    );
+  }
+}
+
+
